d480e85f524b7ab3b12cdb7faa2e697f2e03574a
Michael Lam <lamm@eecs.oregonstate.edu>
2015-03-03 15:49:29 -0800

    Added run_me script

---
+++ run_me.m
@@ -0,0 +1,10 @@
+% Assignment #4
+% CS 536, Winter 2015
+% Author: Michael Lam
+
+%% Constants
+NIPS_DOCWORD_PATH = 'data/docword.nips.txt';
+NIPS_VOCAB_PATH = 'data/vocab.nips.txt';
+KOS_DOCWORD_PATH = 'data/docword.kos.txt';
+KOS_VOCAB_PATH = 'data/vocab.kos.txt';
+
